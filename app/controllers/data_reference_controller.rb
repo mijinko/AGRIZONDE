845ee72e0296ef61b13e701eb696bee7a177fb81
yakitori <pastrisingagain@hotmail.co.jp>
2014-04-30 14:30:41 +0900

    データ参照処理の皮を作成

---
+++ app/controllers/data_reference_controller.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+class DataReferenceController < ApplicationController
+  layout 'common_layout'
+  def index
+    puts 'data_reference_controller index start'
+  end
+
+  def show_data
+    puts 'data_reference_controller show_data start'
+  end
+end
---
+++ app/helpers/data_reference_helper.rb
@@ -0,0 +1,2 @@
+module DataReferenceHelper
+end
